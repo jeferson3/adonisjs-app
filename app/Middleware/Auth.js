c23e1f4c78ca126c59227dcf0c8e4ac522498963
jeferson <jefersongomes333@hotmail.com>
2020-07-23 20:05:09 -0300

    Create Auth.js

---
+++ app/Middleware/Auth.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Response = require('@adonisjs/framework/src/Response')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class Auth {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request, response, auth }, next) {
+    // call next to advance the request
+    if(await auth.user){
+      return await next()
+    }
+    return response.route('welcome');
+  }
+}
+
+module.exports = Auth
