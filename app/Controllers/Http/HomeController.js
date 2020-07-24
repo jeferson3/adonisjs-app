e1cc5d6502808891e6155af40fb3f158420b1d74
jeferson <jefersongomes333@hotmail.com>
2020-07-24 16:26:47 -0300

    Update HomeController.js

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -6,10 +6,14 @@ class HomeController {
     async index({view})
     {
         var products = await Product.all()
-
-        console.log(products.toJSON());
-
         return view.render('welcome', {'products': products.toJSON()});
+    }
+
+    async show({ view, params })
+    {
+        var {id} = params;
+        var product = await Product.find(id);
+        return view.render('single', {'product': product.toJSON()});
     }
 }
 
