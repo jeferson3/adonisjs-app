3351f157cd2530b831e692fefd327f2ab30feb96
jeferson <jefersongomes333@hotmail.com>
2020-07-24 16:26:13 -0300

    Create ProductController.js

---
+++ app/Controllers/Http/admin/ProductController.js
@@ -0,0 +1,105 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with products
+ */
+
+ var Product = use('App/Models/Product')
+
+class ProductController {
+  /**
+   * Show a list of all products.
+   * GET products
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    return view.render('admin.products.index')
+  }
+
+  /**
+   * Render a form to be used for creating a new product.
+   * GET products/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+    return view.render('admin.products.create')
+  }
+
+  /**
+   * Create/save a new product.
+   * POST products
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    return 'store'
+  }
+
+  /**
+   * Display a single product.
+   * GET products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    return view.render('admin.products.show')
+  }
+
+  /**
+   * Render a form to update an existing product.
+   * GET products/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+    var {id} = params;
+    var product = await Product.find(id)
+    return view.render('admin.products.edit', {'product':product.toJSON()})
+  }
+
+  /**
+   * Update product details.
+   * PUT or PATCH products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    return 'update'
+  }
+
+  /**
+   * Delete a product with id.
+   * DELETE products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    return 'destroy'
+  }
+}
+
+module.exports = ProductController
