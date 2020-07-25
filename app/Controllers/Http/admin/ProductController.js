9a48c52b80c9e304cb9802943702dfb9933b0854
jeferson <jefersongomes333@hotmail.com>
2020-07-25 11:06:42 -0300

    Update ProductController.js

--- app/Controllers/Http/admin/ProductController.js
+++ app/Controllers/Http/admin/ProductController.js
@@ -22,8 +22,8 @@ class ProductController {
    */
   async index ({ request, response, view }) {
 
-    var page = request.get();
-    var products = await Product.query().paginate(1, 20);
+    var page = isNaN(request.get().page)? '1': request.get().page;
+    var products = await Product.query().paginate(page);
     var pagination = products.pages;
     console.log(pagination);
     return view.render('admin.products.index', {'products': products.toJSON().data, pagination})
