09aeed0fff3b8a75dbf1cb4488d2789d8df50a50
jeferson <jefersongomes333@hotmail.com>
2020-07-25 15:45:24 -0300

    Create Guest.js

---
+++ app/Middleware/Guest.js
@@ -0,0 +1,21 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class Guest {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request, response, auth }, next) {
+    // call next to advance the request
+    if(auth.user){
+      return response.route('products.index')
+    }
+    return await next()
+  }
+}
+
+module.exports = Guest
