6e098f49cc994a0e08c894a2e0ce25aa7047760d
jeferson <jefersongomes333@hotmail.com>
2020-07-27 19:55:52 -0300

    Update ProductsTableSeeder.js

--- database/seeds/ProductsTableSeeder.js
+++ database/seeds/ProductsTableSeeder.js
@@ -18,12 +18,12 @@ const User = use('App/Models/User')
 class ProductsTableSeeder {
   async run () {
     Factory.model('App/Models/Product').createMany(10)
-    // User.create({
-    //   username: 'jeferson',
-    //   email: 'jeferson@email.com',
-    //   password: '123456'
+    User.create({
+      username: 'jeferson',
+      email: 'jeferson@email.com',
+      password: '123456'
 
-    // })
+    })
   }
 }
 
