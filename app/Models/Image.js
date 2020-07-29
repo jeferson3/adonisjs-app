8108eb05a22252dde123b9ed9830f64f5bf3c129
jeferson <jefersongomes333@hotmail.com>
2020-07-28 21:08:49 -0300

    Update Image.js

--- app/Models/Image.js
+++ app/Models/Image.js
@@ -2,8 +2,18 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+var Env = use('Env')
 
 class Image extends Model {
+
+    // static get computed () {
+    //     return ['url']
+    //   }
+    
+    //   getUrl ({ photo }) {
+    //     return `${Env.get('APP_URL')}/images/${photo}`
+    //   }
+
     products() {
         return this.belongsTo('App/Models/Product')
     }
