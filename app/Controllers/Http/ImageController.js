62069af5f12e371b761ab9b7d2507d30d6386e63
jeferson <jefersongomes333@hotmail.com>
2020-07-29 13:03:48 -0300

    Update ImageController.js

--- app/Controllers/Http/ImageController.js
+++ app/Controllers/Http/ImageController.js
@@ -1,12 +1,28 @@
 'use strict'
 
+
 const Helpers = use('Helpers')
+const Product = use('App/Models/Product')
 
 class ImageController {
     async show({response, params})
     {
         var {photo} = params
         return response.download(Helpers.tmpPath(`uploads/${photo}`))
+
+    }
+    async delete({response, request, params}){
+        var removeFile = Helpers.promisify(require('fs').unlink)
+        var referer = request.headers().referer;
+        var {photo} = params
+        var { id } = params;
+        var product = await Product.find(id)
+        removeFile(Helpers.tmpPath(`uploads/${photo}`))
+        product.images()
+        return response.redirect(referer);
+
+
+
     }
 }
 
