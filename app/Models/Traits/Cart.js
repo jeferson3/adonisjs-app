fc88ce9782842dccc692057dc79b8f2269ffa511
jeferson <jefersongomes333@hotmail.com>
2020-07-30 20:29:48 -0300

    Create Cart.js

---
+++ app/Models/Traits/Cart.js
@@ -0,0 +1,10 @@
+'use strict'
+
+class Cart {
+  register (Model, customOptions = {}) {
+    const defaultOptions = {}
+    const options = Object.assign(defaultOptions, customOptions)
+  }
+}
+
+module.exports = Cart
