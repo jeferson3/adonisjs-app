3e4193632c5ba0789875fc57eb1ece8db5e944d7
jeferson <jefersongomes333@hotmail.com>
2020-07-30 15:01:40 -0300

    Update CartController.js

--- app/Controllers/Http/admin/CartController.js
+++ app/Controllers/Http/admin/CartController.js
@@ -3,19 +3,33 @@
 const Product = use('App/Models/Product')
 
 class CartController {
-    async index({ view }) {
+    async index({ view, session }) {
+        return session.all(); 
+        
         return view.render('admin.cart')
     }
     async store({ request, response, session }) {
-        var { product, qtd } = request.all();
-        var product = await Product.find(product);
+        var { product_id, qtd } = request.all();
+        var id = parseInt(product_id);
 
-        if (!product || product < 1 || typeof id != 'number') {
+        if (isNaN(id) || qtd < 1 || typeof id != 'number') {
             return response.redirect('back')
         }
+        
+        var product = await Product.find(id);
+        if (!product) {
+            return response.redirect('back')
+        }
+        var { name, price } = product;
+        var data = {
+            name,
+            price,
+            qtd,
+            amount: price * qtd
+        }
 
-        // session.push('cart', )
-        return request.all()
+        session.get('cart', data)
+        return response.route('cart.index');
     }
 }
 
