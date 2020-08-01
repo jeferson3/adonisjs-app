ca9d5e69deae87663f63ced68b102736ae4a284a
jeferson <jefersongomes333@hotmail.com>
2020-07-31 22:36:08 -0300

    Update HomeController.js

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var Product = use('App/Models/Product');
+var Category = use('App/Models/Category');
 
 class HomeController {
     
@@ -15,6 +16,18 @@ class HomeController {
         var {slug} = params;
         var product = await Product.query().where('slug', slug).with('images').first();
         return view.render('single', {'product': product.toJSON()});
+    }
+    async category({response, params, view}){
+        var {slug} = params;
+        if (!slug) {
+            response.route('welcome')
+        }
+        var products = await Category.query().with('products').where('slug', slug).first()
+        var nameCategory = products.toJSON().name
+        products = await products.products().with('images').fetch()
+        products = products.toJSON()
+        return view.render('categories', {products, nameCategory});
+        
     }
 }
 
