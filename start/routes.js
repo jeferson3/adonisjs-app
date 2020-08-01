c840664874184e975b9f7141ba9d97a0eb17e634
jeferson <jefersongomes333@hotmail.com>
2020-07-31 22:36:35 -0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -17,6 +17,7 @@
 const Route = use('Route')
 // routes for users
 
+Route.get('/category/:slug', 'HomeController.category').as('category')
 Route.get('', 'HomeController.index').as('welcome')
 Route.get('/:slug', 'HomeController.show').as('product.single').prefix('product')
 
