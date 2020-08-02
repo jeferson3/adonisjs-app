436a27ec4c6a98a37dc625a613e9517e4ef28431
jeferson <jefersongomes333@hotmail.com>
2020-08-02 12:56:24 -0300

    Update UserController.js

--- app/Controllers/Http/admin/UserController.js
+++ app/Controllers/Http/admin/UserController.js
@@ -7,6 +7,9 @@ class UserController {
         var users = await User.query().setHidden(['password']).fetch()
         return view.render('admin.users.index', {'users':users.toJSON()})
     }
+    async create({view}){
+        return view.render('admin.users.create')
+    }
 }
 
 module.exports = UserController
