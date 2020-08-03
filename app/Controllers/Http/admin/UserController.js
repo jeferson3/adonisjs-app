5b804d46db484fb330586c737fa7de08c48b4518
jeferson <jefersongomes333@hotmail.com>
2020-08-03 11:38:05 -0300

    Update UserController.js

--- app/Controllers/Http/admin/UserController.js
+++ app/Controllers/Http/admin/UserController.js
@@ -10,6 +10,28 @@ class UserController {
     async create({view}){
         return view.render('admin.users.create')
     }
+    async store({request, response, view}){
+        var user = request.except('_csrf')
+        await User.create(user)
+        return response.route('users.index');
+    }
+    async edit({view, params}){
+        var {id} = params
+        var user = await User.query().where('id', id).first()
+        return view.render('admin.users.edit', {user})
+    }
+    async update({request, response, params}){
+        var {id} = params
+        var newUser = request.except('_csrf');
+        var user = await User.query().where('id', id).first()
+        user.update(newUser)
+        return response.route('admin.users.index');
+    }
+    async destroy({request, response, params}){
+        var {id} = params
+        await User.query().where('id', id).delete()
+        return response.route('admin.users.index');
+    }
 }
 
 module.exports = UserController
