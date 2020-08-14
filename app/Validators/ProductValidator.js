2fcabb189da9087a4f07cbf1adaf9bba8315d7f2
jeferson <jefersongomes333@hotmail.com>
2020-08-14 18:29:59 -0300

    update ProductValidator.js

--- app/Validators/ProductValidator.js
+++ app/Validators/ProductValidator.js
@@ -1,20 +1,20 @@
 'use strict'
 
 class ProductValidator {
-  get rules () {
+  get rules() {
     return {
       // validation rules
-      'name': 'required|min:5|unique:products, id',
+      'name': 'required|min:5|unique:products, name',
       'price': 'required',
-      'description':'required|min:10',
-      'category':'required'
+      'description': 'required|min:10',
+      'category': 'required'
     }
   }
 
-  get messages(){
-    return{
+  get messages() {
+    return {
       'required': 'Este campo é obrigatório!',
-      'min':'Este campo deve ter no mínimo {{argument}} caracteres!',
+      'min': 'Este campo deve ter no mínimo {{argument}} caracteres!',
       'unique': 'Este produto já existe, use um nome diferente!'
     }
   }
