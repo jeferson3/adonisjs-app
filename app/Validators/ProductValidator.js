2dde5b1b2005417906f39f11b0a8f86573cbb960
jeferson <jefersongomes333@hotmail.com>
2020-08-14 21:25:57 -0300

    make it better

--- app/Validators/ProductValidator.js
+++ app/Validators/ProductValidator.js
@@ -6,18 +6,18 @@ class ProductValidator {
     var data = id ? `name, id, ${id}` : 'name';
     return {
       // validation rules
-      // 'name': `required|min:5|unique:products, ${data}`,
-      // 'price': 'required',
-      // 'description': 'required|min:10',
-      // 'category': 'required'
+      'name': `required|min:5|unique:products, ${data}`,
+      'price': 'required',
+      'description': 'required|min:10',
+      'category': 'required'
     }
   }
 
   get messages() {
     return {
-      // 'required': 'Este campo é obrigatório!',
-      // 'min': 'Este campo deve ter no mínimo {{argument}} caracteres!',
-      // 'unique': 'Este produto já existe, use um nome diferente!'
+      'required': 'Este campo é obrigatório!',
+      'min': 'Este campo deve ter no mínimo {{argument}} caracteres!',
+      'unique': 'Este produto já existe, use um nome diferente!'
     }
   }
 }
